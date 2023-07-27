e3a128a8b76e4b0b776200b2738c6a51db3e357a
stew5555 <stewartyousif1@gmail.com>
2023-07-26 23:26:43 -0700

    installed bootstrap, and added navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-
-
+        <nav className="navbar">
+          <div className="container-fluid justify-content-center">
+            <form className="d-flex input-group-lg" role="search">
+              <input className="form-control me-4" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </nav>
+        <div className='container'></div>
       </header>
     </div>
   );
