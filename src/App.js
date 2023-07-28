27c22ba948442ebe681791240a05447acc7c1caa
stew5555 <stewartyousif1@gmail.com>
2023-07-27 21:44:27 -0700

    imported Countires.js file to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Countries from './components/Countries';
 
 
 function App() {
@@ -16,13 +17,7 @@ function App() {
           </div>
         </nav>
         <div className='container'>
-          <div className="card" style={{width: '18rem'}}>
-            <img src="./public/flags/ad.png" className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-          </div>
+          <Countries />
         </div>
       </header>
     </div>
