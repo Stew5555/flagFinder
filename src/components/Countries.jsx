1cbda33ca18b8ea702a63d3a82f3072865e8e809
stew5555 <stewartyousif1@gmail.com>
2023-07-30 19:37:55 -0700

    fixed an issue with grid, and overlapping cards

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,25 +3,26 @@ import countries from "../countries.json"
 
 function Countries({ searchQuery }) {
     return (
-        <div className="row">
+        // <div className="main">
+
+            <div className="grid-container">
                 {
                     countries.filter(country => country.name.toLowerCase().includes(searchQuery.toLowerCase())).map((country) => {
                         const flagUrl = `/flags/${country.alpha2}.png`;
 
 
                         return (
-                            <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={country.id}>
-                                <div className="card" style={{ width: '18rem' }}>
-                                    <img src={flagUrl} className="card-img-top" alt={`${country.name}'s flag`} />
-                                    <div className="card-body">
-                                        <p className="card-text">{country.name}</p>
-                                    </div>
+                            <div className="card" style={{ width: '18rem', height: '22rem' }} key={country.id}>
+                                <img src={flagUrl} className="card-img-top" alt={`${country.name}'s flag`} />
+                                <div className="card-body">
+                                    <p className="card-text">{country.name}</p>
                                 </div>
                             </div>
                         )
                     })
                 }
-        </div>
+            </div>
+        // </div>
     )
 }
 
